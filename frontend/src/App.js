d895e188477ddedf8355ceae86390f227fdb7885
Amanda Simmons <carpe737diem@gmail.com>
2021-04-01 11:24:11 -0400

    successfully routing between two pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
 
-function App() {
+const Header = () => {
+  return (
+    <div>
+      <p>Header</p>
+    </div>
+  )
+};
+
+const Homepage = () => {
   const [posts, setPosts] = useState([])
   const url = 'http://127.0.0.1:8000/api/posts/'
   useEffect(() => {
@@ -10,8 +19,21 @@ function App() {
       .then(res => res.json())
       .then(data => setPosts(data));
   }, [])
+
+  // const boasts = () => {
+  //   const [boasts, setBoasts] = useState([])
+  //   const url = 'http://127.0.0.1:8000/api/posts/boasts'
+  //   useEffect(() => {
+  //     fetch(url)
+  //       .then(res => res.json())
+  //      .then(data => setBoasts(data));
+  //   }, [])
+  // }
+
   return (
     <>
+      <Header/>
+      <Link to='/about'>Go to Aboutpage</Link>
       <h1>Ghost Post Machine</h1>
       {posts.map ((p)=> (
       <ul>
@@ -27,4 +49,14 @@ function App() {
   );
 }
 
-export default App;
+const Aboutpage = () => {
+  return (
+    <div>
+      <Header/>
+      <h1>Aboutpage</h1>
+      <Link to='/'>Go to Homepage</Link>
+    </div>
+  )
+};
+
+export {Homepage, Aboutpage};
